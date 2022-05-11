8eb8defaba5d55a7adb8ef14c2a1c3baf7142fa0
YagoESP <santiagogarciag732015@gmail.com>
2022-05-11 15:56:36 +0200

    commit

--- js/desktop/accordion.js
+++ js/desktop/accordion.js
@@ -5,7 +5,6 @@ export let renderAccordion = () =>{
     if(faqs){
         faqs.forEach((faq, i) => {
             faq.addEventListener("click", () => {
-                faqs[i].classList.toggle("active")
                 let contents = document.querySelectorAll(".content")
                 contents[i].classList.toggle("active")
                 let arrow = document.querySelectorAll(".item")
